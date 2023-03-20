5a67dd5c0b84ab85f269ec4d211d0d6a38f2edfa
Sathwik Reddy <sathwik.kotla@zemosolabs.com>
2023-03-20 15:12:11 +0530

    qfactory design

---
+++ src/factory_method_design/Calculator.java
@@ -0,0 +1,8 @@
+package Factory_Method_Design;
+
+public interface Calculator {
+	public double add(double x, double y);
+    public double subtract(double x, double y);
+    public double multiply(double x, double y);
+    public double divide(double x, double y);
+}
